ea350b4532c3350eb3e43b94a570357126e970fe
Justin Calleja <justin.calleja@williamhill.com>
2023-02-03 08:55:20 +0100

    wip

--- src/config.mjs
+++ src/config.mjs
@@ -1,15 +1,13 @@
 import defaultImage from './assets/images/default.png';
 
 const CONFIG = {
-  name: 'AstroWind',
-
-  origin: 'https://astrowind.vercel.app',
+  name: 'Justin Calleja',
+  origin: 'https://justincalleja.com',
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'AstroWind — Free template for create a website with Astro + Tailwind CSS',
-  description:
-    '🚀 Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.',
+  title: 'This is the title in src/config.mjs',
+  description: 'This is the description in src/config.mjs',
   defaultImage: defaultImage,
 
   defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
